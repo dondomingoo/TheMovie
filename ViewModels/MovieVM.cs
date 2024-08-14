ba96d1d35d21d9365dab622cea553eae3180b79c
Jakob Seeberg <jbse59225@edu.ucl.dk>
2024-08-14 18:55:29 +0200

    wip

---
+++ ViewModels/MovieVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMovie.ViewModels
+{
+    public class MovieVM
+    {
+        public Movie Movie { get; }
+        public string Name { get; set; }
+        public int Duration { get; set; }
+        public string Genre { get; set; }
+
+        public MovieVM(Movie Movie)
+        {
+            this.Movie = Movie;
+            Name = Movie.Name;
+            Duration = Movie.Duration;
+            Genre = Movie.Genre;
+        }
+
+    }
+}
---
+++ ViewModels/MoviesVM.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMovie.ViewModels
+{
+    public class MoviesVM
+    {
+        public ObservableCollection<MovieVM> mVMs { get; set; }
+
+        public MoviesVM() 
+        {
+            mVMs = [];
+            foreach (Movie movie in MovieRepository.Movies)
+            {
+                mVMs.Add(new MovieVM(movie));
+            }
+        }
+
+        public void AddMovie()
+        {
+            Movie movie = new Movie("Titel", 0, "Genre");
+            MovieVM mVM = new(movie);
+            mVMs.Add(mVM);
+            SelectedRoom = rVM;
+        }
+
+        public void SaveMovies()
+        {
+        }
+    }
+}
