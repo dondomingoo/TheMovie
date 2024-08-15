1632b237c86595aed882c086d3d40f01914e4c9f
markus <modj62361@edu.ucl.dk>
2024-08-15 09:24:58 +0200

    movie class

---
+++ Model/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMovie.Model
+{
+    internal class Movie
+    {
+        public string Name { get; set; }
+        public TimeSpan Duration { get; set; }
+        public string Genre { get; set; }
+
+        public Movie(string name, TimeSpan duration, string genre)
+        {
+            Name = name;
+            Duration = duration;
+            Genre = genre;
+        }
+    }
+}
