25270cc7571a12e511f1db2be943162ad3ceb03e
markus <modj62361@edu.ucl.dk>
2024-08-15 09:32:59 +0200

    repository move

--- Model/MovieRepository.cs
+++ Model/MovieRepository.cs
@@ -5,9 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using TheMovie.Model;
 
-namespace TheMovies.Model
+namespace TheMovie.Model
 {
     public class MovieRepository
     {
