006e2d5258aa80df2eeb0f2b62b8fdc86286bfc8
Jakob Seeberg <jbse59225@edu.ucl.dk>
2024-08-20 17:10:35 +0200

    ..

--- Model/DataHandler.cs
+++ Model/DataHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,32 @@ namespace TheMovie.Model
 {
     internal class DataHandler
     {
+        public static string[] LoadFromFile(string fileName)
+        {
+            if (!File.Exists(fileName))
+            {
+                using StreamWriter sw = new(fileName);
+            }
+
+            using StreamReader sr = new(fileName);
+            string[] lines = sr.ReadToEnd().Split(new string[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+
+            return lines;
+        }
+        public static void SaveDataFile<T>(List<T> entities, string fileName) where T : IEntity
+        {
+            try
+            {
+                using StreamWriter sw = new(fileName);
+                foreach (IEntity entity in entities)
+                {
+                    sw.WriteLine(entity.ToString());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
     }
 }
