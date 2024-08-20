1c15289b184b62e53d677fb033e4eaaa268ca223
Jakob Seeberg <jbse59225@edu.ucl.dk>
2024-08-20 17:08:14 +0200

    wip

--- Model/Movie.cs
+++ Model/Movie.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TheMovie.Model
 {
-    public class Movie
+    public class Movie: IEntity
     {
         public string Title { get; set; }
         public int Duration { get; set; }
